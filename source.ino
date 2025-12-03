22a3729fd09cacf8de379cc2de06a1ec867f5b24
utopiasz <s3ndyy3@gmail.com>
2025-12-03 18:58:32 +0100

    fix

--- source.ino
+++ source.ino
@@ -2,35 +2,35 @@
 
 #define HT_ADDR 0x70
 
-// Segmenty liter - dopasowane do 7-seg
+// Segments for letters - mapped to 7-segment display
 uint8_t segBlank = 0x00;
-uint8_t segA = 0b01110111;  // A
 uint8_t segC = 0b00111001;  // C
-uint8_t segD = 0b01011110;  // D (d małe)
+uint8_t segD = 0b01011110;  // D (lowercase)
 uint8_t segE = 0b01111001;  // E
+uint8_t segF = 0b01110001;  // F
 uint8_t segG = 0b01111101;  // G
 
-// Inicjalizacja HT16K33
+// Initialize HT16K33
 void initDisplay() {
   Wire.begin();
 
   Wire.beginTransmission(HT_ADDR);
-  Wire.write(0x21);     // włącz oscylator
+  Wire.write(0x21);     // enable oscillator
   Wire.endTransmission();
 
   Wire.beginTransmission(HT_ADDR);
-  Wire.write(0x81);     // włącz wyświetlacz
+  Wire.write(0x81);     // enable display
   Wire.endTransmission();
 
   Wire.beginTransmission(HT_ADDR);
-  Wire.write(0xEF);     // jasność max
+  Wire.write(0xEF);     // max brightness
   Wire.endTransmission();
 }
 
-// Wyslanie segmentów na 4 cyfry
+// Send segments to 4 digits
 void sendSegments(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3) {
   Wire.beginTransmission(HT_ADDR);
-  Wire.write(0x00); // adres RAM
+  Wire.write(0x00); // RAM address
 
   Wire.write(d0); Wire.write(0x00);
   Wire.write(d1); Wire.write(0x00);
@@ -40,15 +40,15 @@ void sendSegments(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3) {
   Wire.endTransmission();
 }
 
-// pokazanie litery nuty
+// Show note letter
 void showNoteChar(char n) {
   uint8_t s;
 
   switch(n) {
-    case 'A': s = segA; break;
     case 'C': s = segC; break;
     case 'D': s = segD; break;
     case 'E': s = segE; break;
+    case 'F': s = segF; break;
     case 'G': s = segG; break;
     default:  s = segBlank;
   }
@@ -56,28 +56,29 @@ void showNoteChar(char n) {
   sendSegments(s, segBlank, segBlank, segBlank);
 }
 
-// Piny przycisków nut
+// Note button pins
 int cNote = 9;  // C
 int dNote = 8;  // D
 int eNote = 7;  // E
-int gNote = 6;  // G
-int aNote = 5;  // A
+int fNote = 6;  // F
+int gNote = 5;  // G
 
-// Piny kontrolne
+// Control pins
 int Piezo = 2;
-int recordButton = A3;  // Przycisk RECORD
-int playButton = A2;    // Przycisk PLAY
-int redLED = 10;        // Czerwona dioda - nagrywanie
-int greenLED = 11;      // Zielona dioda - odtwarzanie
+int recordButton = A3;  // RECORD button
+int playButton = A2;    // PLAY button
+int redLED = 10;        // Red LED - recording
+int greenLED = 11;      // Green LED - playback
 
-// Częstotliwości nut
+// Note frequencies
 double c = 261.63;
 double d = 293.66;
 double e = 329.63;
+double f = 349.23;
 double g = 392;
 double a = 440;
 
-// Struktura do przechowywania melodii
+// Structure for storing melody
 #define MAX_NOTES 100
 struct Note {
   double frequency;
@@ -93,18 +94,18 @@ unsigned long noteStartTime = 0;
 double currentFrequency = 0;
 
 void setup() {
-  // Przyciski nut
+  // Note buttons
   pinMode(cNote, INPUT);
   pinMode(dNote, INPUT);
   pinMode(eNote, INPUT);
+  pinMode(fNote, INPUT);
   pinMode(gNote, INPUT);
-  pinMode(aNote, INPUT);
   
-  // Przyciski kontrolne
+  // Control buttons
   pinMode(recordButton, INPUT_PULLUP);
   pinMode(playButton, INPUT_PULLUP);
 
-  // LEDy
+  // LEDs
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
 
@@ -113,26 +114,26 @@ void setup() {
   Serial.begin(9600);
 
   initDisplay();
-  showNoteChar(' '); // na start pusto
+  showNoteChar(' '); // blank on start
 
-  Serial.println("Piano gotowe!");
+  Serial.println("Piano ready!");
 }
 
 void loop() {
-  // Obsługa przycisku RECORD
+  // Handle RECORD button
   if (digitalRead(recordButton) == LOW && !recordButtonPressed) {
     recordButtonPressed = true;
     isRecording = !isRecording;
     
     if (isRecording) {
       noteCount = 0;
-      digitalWrite(redLED, HIGH);  // Zapal czerwoną diodę
-      Serial.println("*** NAGRYWANIE ROZPOCZETE ***");
+      digitalWrite(redLED, HIGH);  // Turn on red LED
+      Serial.println("*** RECORDING STARTED ***");
     } else {
-      digitalWrite(redLED, LOW);   // Zgaś czerwoną diodę
-      Serial.print("*** NAGRYWANIE ZAKONCZONE - zapisano ");
+      digitalWrite(redLED, LOW);   // Turn off red LED
+      Serial.print("*** RECORDING FINISHED - saved ");
       Serial.print(noteCount);
-      Serial.println(" nut ***");
+      Serial.println(" notes ***");
     }
     delay(200);
   }
@@ -146,38 +147,38 @@ void loop() {
   }
   if (digitalRead(playButton) == HIGH) playButtonPressed = false;
 
-  // ---------------- NUTY ----------------
+  // ---------------- NOTES ----------------
   double freq = 0;
   char noteChar = ' ';
 
   if (digitalRead(cNote) == 1) {
     freq = c; noteChar = 'C';
-    Serial.println("Nacisnieto przycisk: C");
+    Serial.println("Button pressed: C");
   } else if (digitalRead(dNote) == 1) {
     freq = d; noteChar = 'D';
-    Serial.println("Nacisnieto przycisk: D");
+    Serial.println("Button pressed: D");
   } else if (digitalRead(eNote) == 1) {
     freq = e; noteChar = 'E';
-    Serial.println("Nacisnieto przycisk: E");
+    Serial.println("Button pressed: E");
+  } else if (digitalRead(fNote) == 1) {
+    freq = f; noteChar = 'F';
+    Serial.println("Button pressed: F");
   } else if (digitalRead(gNote) == 1) {
     freq = g; noteChar = 'G';
-    Serial.println("Nacisnieto przycisk: G");
-  } else if (digitalRead(aNote) == 1) {
-    freq = a; noteChar = 'A';
-    Serial.println("Nacisnieto przycisk: A");
+    Serial.println("Button pressed: G");
   }
 
-  // pokaz nutę na wyświetlaczu
+  // Show note on display
   showNoteChar(noteChar);
 
-  // dźwięk
+  // Sound
   if (freq > 0) {
     tone(Piezo, freq);
     
-    // Nagrywanie - rozpoczęcie nowej nuty
+    // Recording - start new note
     if (isRecording && freq != currentFrequency) {
       if (currentFrequency > 0 && noteCount < MAX_NOTES) {
-        // Zapisz poprzednią nutę
+        // Save previous note
         melody[noteCount].frequency = currentFrequency;
         melody[noteCount].duration = millis() - noteStartTime;
         noteCount++;
@@ -188,7 +189,7 @@ void loop() {
   } else {
     noTone(Piezo);
     
-    // Nagrywanie - zakończenie nuty
+    // Recording - end note
     if (isRecording && currentFrequency > 0 && noteCount < MAX_NOTES) {
       melody[noteCount].frequency = currentFrequency;
       melody[noteCount].duration = millis() - noteStartTime;
@@ -202,16 +203,16 @@ void loop() {
 
 void playMelody() {
   if (noteCount == 0) {
-    Serial.println("Brak nagranej melodii!");
-    // Sygnał dźwiękowy
+    Serial.println("No recorded melody!");
+    // Audio signal
     tone(Piezo, 200, 100);
     delay(150);
     tone(Piezo, 200, 100);
     return;
   }
   
-  Serial.println("*** ODTWARZANIE (Tempo 0.5s) ***");
-  digitalWrite(greenLED, HIGH);  // Zapal zieloną diodę
+  Serial.println("*** PLAYBACK (Tempo 0.5s) ***");
+  digitalWrite(greenLED, HIGH);  // Turn on green LED
   noTone(Piezo);
   delay(500);
   
@@ -221,8 +222,8 @@ void playMelody() {
     if (melody[i].frequency == c) noteChar = 'C';
     if (melody[i].frequency == d) noteChar = 'D';
     if (melody[i].frequency == e) noteChar = 'E';
+    if (melody[i].frequency == f) noteChar = 'F';
     if (melody[i].frequency == g) noteChar = 'G';
-    if (melody[i].frequency == a) noteChar = 'A';
 
     showNoteChar(noteChar);
 
